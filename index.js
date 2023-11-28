16fbb90dbc451a8e0910e7d69f39310e75b2f57d
Billie Redwood <billieredwood@gmail.com>
2023-11-28 15:43:08 +0000

    1st part of mvp completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// console.log("hello");
+
+const getCountryByName = async (countryName) => {
+    const response = await fetch("https://restcountries.com/v3.1/name/" +countryName);
+    const data = await response.json();
+ 
+    console.log(data);
+
+}
+
+
+getCountryByName("Norway");
+
