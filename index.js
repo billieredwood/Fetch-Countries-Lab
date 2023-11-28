b279279dd773e2498d5dabf17fab062349fed539
Billie Redwood <billieredwood@gmail.com>
2023-11-28 16:49:37 +0000

    completed the mvp with task 2 testing as expected.

--- index.js
+++ index.js
@@ -3,11 +3,31 @@
 const getCountryByName = async (countryName) => {
     const response = await fetch("https://restcountries.com/v3.1/name/" +countryName);
     const data = await response.json();
- 
-    console.log(data);
-
+    displayData(data)
 }
 
 
-getCountryByName("Norway");
+const section = document.querySelector("#countries")
+const displayData = (data) => {
+    const name = document.createElement("p")
+    name.innerText = "Name: " + data[0].name.common
+    section.appendChild(name);
 
+    const capital = document.createElement("p")
+    capital.innerText = "Capital: " + data[0].capital[0]
+    section.appendChild(capital);
+
+    const languages = document.createElement("p")
+    languages.innerText = "Languages: " + Object.values(data[0].languages).join(", ")
+    section.appendChild(languages);
+
+    const population = document.createElement("p")
+    population.innerText = "Population: " + data[0].population
+    section.appendChild(population);
+
+console.log(population);
+
+
+}
+
+getCountryByName("Norway");
